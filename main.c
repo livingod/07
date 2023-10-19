f3abaa010531777cba624bcba5d32822cd1d93cc
livingod <144990631+livingod@users.noreply.github.com>
2023-10-19 15:44:43 +0900

    03

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int inc(int counter)
 {
-  int i;
-  int temp = 1;
-  
-  for(i=0;i<5;i++)//5beon dol a~ 
-  {
-    //static int temp = 1
-    printf("temp = %d\n",temp);
-    temp++;
+    counter++;
+    return counter;
 }
 
+int main(int argc, char *argv[])
+{
+  int i = 10;
+  printf("hamsoo hochul jeon i=%d\n",i);
+  
+  i = inc(i);//oh wow~
+  printf("hamsoo hochul hu i=%d\n",i);
+  
   system("PAUSE");	
   return 0;
 }
+
+//call by vlaue !!!!!!!!!!!!!!!!!!t
